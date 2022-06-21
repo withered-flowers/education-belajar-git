2e79f7b237d809c196629d2d2c3171555056f134
Withered Flowers <withered-flowers@users.noreply.github.com>
2022-06-21 09:31:08 +0700

    fix: adding middleware for app.js

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
+const cors = require("cors");
 const express = require("express");
 const app = express();
 
 const port = 3000;
+
+// middleware
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 app.listen(port, (_) => console.log(`Apps is listening at ${port}`));
